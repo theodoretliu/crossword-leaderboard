c57fead1a29cb9023b80b521268103279515da81
Theodore Liu <theodoretliu@gmail.com>
2023-11-14 00:35:14 -0800

    Package all backend updates

--- server/feature_flag.go
+++ server/feature_flag.go
@@ -1,15 +1,16 @@
 package main
 
 func GetFeatureFlag(flagName string) (bool, error) {
-	row := db.QueryRow("SELECT status FROM feature_flags WHERE flag = ?;", flagName)
+	return false, nil
+	// row := db.QueryRow("SELECT status FROM feature_flags WHERE flag = ?;", flagName)
 
-	var b bool
+	// var b bool
 
-	err := row.Scan(&b)
+	// err := row.Scan(&b)
 
-	if err != nil {
-		return false, err
-	}
+	// if err != nil {
+	// 	return false, err
+	// }
 
-	return b, nil
+	// return b, nil
 }
