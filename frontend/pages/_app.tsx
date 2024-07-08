8dde6204679f4aa2ecff651540b03d26e39f5b95
Theodore Liu <theodoretliu@gmail.com>
2024-07-08 00:25:32 -0700

    Install posthog

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,19 +1,50 @@
 import "@/global.css";
 import "@/fonts.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useRouter } from "next/router";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <React.Fragment>
-      <Head>
-        <title>Teddy's Mini Leaderboard</title>
-      </Head>
-      <TooltipProvider>
-        <Component {...pageProps} />
-      </TooltipProvider>
-    </React.Fragment>
+    <PostHogProvider client={posthog}>
+      <React.Fragment>
+        <Head>
+          <title>Teddy's Mini Leaderboard</title>
+        </Head>
+        <TooltipProvider>
+          <Component {...pageProps} />
+        </TooltipProvider>
+      </React.Fragment>
+    </PostHogProvider>
   );
 }
